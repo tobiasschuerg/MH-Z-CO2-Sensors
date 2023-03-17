5c6c2b5b69b2bb6d0ef7a9ed1ce9d37ca23d1881
dominik <dominik.basner@gmail.com>
2023-03-17 11:31:23 +0100

    handling invalid pwm lengths#

--- MHZ.cpp
+++ MHZ.cpp
@@ -46,7 +46,8 @@ void IRAM_ATTR pulseInInterruptHandler(){
 	else { // End of pulse
 		unsigned long th = getTimeDiff(sPulseInStartMillis, millis());
 		if (th > 1004 || th < 1) {
-			return 0;
+			sLastPwmPpm = 0;
+			return;
 		}
 		unsigned long tl = 1004 - th;
 		sLastPwmPpm = sRange * (th - 2) / (th + tl - 4);		
